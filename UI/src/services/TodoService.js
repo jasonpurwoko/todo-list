40509064a1ee4e9d09167f717b7e5b4c5c4bc827
jasonpurwoko <itspurwoko@gmail.com>
2021-08-09 14:09:06 -0500

    Create Update REST API and Add Bootstrap

--- UI/src/services/TodoService.js
+++ UI/src/services/TodoService.js
@@ -3,12 +3,19 @@ import axios from 'axios';
 const TODO_API_BASE_URL = "http://localhost:8080/api/v1/todos";
 
 class TodoService {
-
-    getTodos(){
+    getTodos() {
         return axios.get(TODO_API_BASE_URL);
     }
 
-    deleteTodo(todoId){
+    createTodo(todo) {
+        return axios.post(TODO_API_BASE_URL, todo);
+    }
+
+    updateTodo(todo, todoId) {
+        return axios.put(TODO_API_BASE_URL + '/' + todoId, todo);
+    }
+
+    deleteTodo(todoId) {
         return axios.delete(TODO_API_BASE_URL + '/' + todoId);
     }
 }
